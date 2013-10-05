d85de14417299f669f54680ef7043890b5901760
Patrikkk <patrikveres@hotmail.com>
2013-10-05 11:12:54 +0200

    Updated to tshock 4.2

---
+++ AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// Assembly CloneChecker, Version 1.0.0.0
+
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 Microsoft 2011")]
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Reflection.AssemblyTitle("MoreAdminCommands")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("Microsoft")]
+[assembly: System.Reflection.AssemblyProduct("MoreAdminCommands")]
+[assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v4.0", FrameworkDisplayName=".NET Framework 4")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("84db775e-14d9-4bab-aed1-c98df5cb8086")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+
